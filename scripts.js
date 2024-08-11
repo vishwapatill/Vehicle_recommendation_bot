9e739daa4279a8db52a1c9e706e2c9e2a7192498
vishwapatill <142089644+vishwapatill@users.noreply.github.com>
2024-08-11 14:20:36 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 
-const API_KEY = "AIzaSyCbAg1sz5PZ17oWsbmfAO8i6ZrwtoSwNig";
+const API_KEY = "your api key";
 const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
